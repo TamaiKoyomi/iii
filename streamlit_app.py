0c00ae3111fca9f1ca7454dce6df92cfade602ac
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-05-08 07:46:49 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ st.title("BMI計算アプリ")
 user_height=st.number_input("あなたの身長を入力してください(単位はm)。")
 user_weight=st.number_input("あなたの体重を入力してください(単位はkg)。")
 
-a=user_height*user_height
-b=user_weight/a
+a=user_weight/user_height/user_height
 
-st.write("あなたのBMIは"+str(b)+"です")
+st.write("あなたのBMIは"+str(a)+"です")
