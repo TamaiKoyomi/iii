4284c73645b5861f8eaab955aede9c7b99a7366e
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-05-08 07:43:59 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,10 @@
 import streamlit as st
-import random
 
-st.title("おみくじアプリ")
+st.title("BMI計算アプリ")
 
-user_name=st.text_input("あなたの名前を入力してください。")
+user_height=st.number_input("あなたの身長を入力してください(単位はm)。")
+user_weight=st.number_input("あなたの体重を入力してください(単位はkg)。")
 
-if st.button("おみくじを引く"):
-    results=["大吉","中吉","小吉","吉","凶","大凶"]
-    result=random.choice(results)
+bmi=user_weight/user_height/user_height
 
-    comments={
-        "大吉":"素晴らしい一日になりそうです！",
-        "中吉":"良いことがあるかもしれません。",
-        "小吉":"悪くはなさそうですね。",
-        "吉":"まあまあの一日になりそうです。",
-        "凶":"注意して過ごしてください。",
-        "大凶":"今日は特に注意が必要です！"
-    }
-
-    if user_name:
-        st.write(f"{user_name}さんの結果:{result}")
-    else:
-        st.write(f"結果:{result}")
-
-    st.write(comments[result])
+st.write("あなたのBMIは"+str(bmi)+"です")
