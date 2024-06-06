3f9723d6064adf066fc1b0f7c09d2bab25d2a6a6
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-06-06 08:30:52 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,28 @@ def load_data():
 words_df = load_data()
 
 if st.button('四字熟語を見る'):
-    print(words_df)
+    rarity_probs = {
+        'N': 0.4,
+        'R': 0.3,
+        'SR': 0.2,
+        'SSR': 0.1
+    }
+    chosen_rarity = np.random.choice(list(rarity_probs.keys()), p=list(rarity_probs.values()))
+    subset_df = words_df[words_df['レアリティ'] == chosen_rarity]
+    selected_word = subset_df.sample().iloc[0]
+    
+    # セッションステートに選択された単語を保存
+    st.session_state.selected_word = selected_word
+    st.session_state.display_meaning = False
+
+if 'selected_word' in st.session_state:
+    st.header(f"単語名: {st.session_state.selected_word['単語']}")
+    st.subheader(f"読み方:{st.session_state.selected_word['読み方']}")
+    st.subheader(f"レアリティ: {st.session_state.selected_word['レアリティ']}")
+
+    # 意味を確認するボタンを追加
+    if st.button('意味を確認する'):
+        st.session_state.display_meaning = True
+
+    if st.session_state.display_meaning:
+        st.write(f"意味: {st.session_state.selected_word['意味']}")
