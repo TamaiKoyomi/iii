2b12a210c612ebb84db78016ab9b67d6ce68856e
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-07-05 09:07:56 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def load_data():
 words_df = load_data()
 
 def judge(kategori):
-    if st.button(kategori) and st.session_state.selected_word['分類']==kategori:
+    if st.session_state.selected_word['分類']==kategori:
             return True
     else:
         return False
@@ -40,46 +40,33 @@ if 'selected_word' in st.session_state:
 
     st.write('この四字熟語は、次の選択肢のうちどれに分類されるでしょう？(Chat GPTが分類しました。違うと思っても怒らないでください。)')
 
-    kategori=['文学・哲学的なテーマ性','行動・精神的な特性','自然・現象に関連するもの']
-
-    for bunrui in kategori:
-        if judge==True:
-            st.write('正解です。おめでとうございます！正確な意味も確認しましょう。')
-            if st.button('意味を確認する'):
-                st.session_state.display_meaning = True
-
-            if st.session_state.display_meaning:
-                st.write(f"意味: {st.session_state.selected_word['意味']}")
-
-        elif judge==False:
-            st.write('残念、違います。正しい意味を確認して、この四字熟語をマスターしましょう！')
-            if st.button('意味を確認する'):
-                st.session_state.display_meaning = True
-
-            if st.session_state.display_meaning:
-                st.write(f"意味: {st.session_state.selected_word['意味']}")
-
-
-
-
-'''    if st.button('文学・哲学的なテーマ性') and st.session_state.selected_word['分類']=='文学・哲学的なテーマ性'
+    if st.button('文学・哲学的なテーマ性') and judge('文学・哲学的なテーマ性')==True:
         st.write('正解です。おめでとうございます！正確な意味も確認しましょう。')
         if st.button('意味を確認する'):
             st.session_state.display_meaning = True
 
         if st.session_state.display_meaning:
             st.write(f"意味: {st.session_state.selected_word['意味']}")
-    elif st.button('行動・精神的な特性') and st.session_state.selected_word['分類']=='行動・精神的な特性':
+    elif st.button('行動・精神的な特性') and judge('行動・精神的な特性')==True:
         st.write('正解です。おめでとうございます！正確な意味も確認しましょう。')
         if st.button('意味を確認する'):
             st.session_state.display_meaning = True
 
         if st.session_state.display_meaning:
             st.write(f"意味: {st.session_state.selected_word['意味']}")
-    elif st.button('自然・現象に関連するもの') and st.session_state.selected_word['分類']=='自然・現象に関連するもの':
+    elif st.button('自然・現象に関連するもの') and judge('自然・現象に関連するもの')==True:
         st.write('正解です。おめでとうございます！正確な意味も確認しましょう。')
         if st.button('意味を確認する'):
             st.session_state.display_meaning = True
 
         if st.session_state.display_meaning:
-            st.write(f"意味: {st.session_state.selected_word['意味']}")'''
+            st.write(f"意味: {st.session_state.selected_word['意味']}")
+    elif judge==False:
+        st.write('残念、不正解です。')
+        st.write(f"正解はこちら：{st.session_state.selected_word['分類']}")
+        st.write('正しい答えを確認し、この熟語をマスターしましょう！')
+        if st.button('意味を確認する'):
+            st.session_state.display_meaning = True
+
+        if st.session_state.display_meaning:
+            st.write(f"意味: {st.session_state.selected_word['意味']}")
