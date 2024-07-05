8ec1acba3646942108584604d35a6e6974ca1377
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-07-05 08:46:30 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,29 @@ if 'selected_word' in st.session_state:
         else:
             return False
         
-'''    if st.button('文学・哲学的なテーマ性') and st.session_state.selected_word['分類']=='文学・哲学的なテーマ性':
+    kategori=['文学・哲学的なテーマ性','行動・精神的な特性','自然・現象に関連するもの']
+
+    for bunrui in kategori:
+        if judge==True:
+            st.write('正解です。おめでとうございます！正確な意味も確認しましょう。')
+            if st.button('意味を確認する'):
+                st.session_state.display_meaning = True
+
+            if st.session_state.display_meaning:
+                st.write(f"意味: {st.session_state.selected_word['意味']}")
+
+        elif judge==False:
+            st.write('残念、違います。正しい意味を確認して、この四字熟語をマスターしましょう！')
+            if st.button('意味を確認する'):
+                st.session_state.display_meaning = True
+
+            if st.session_state.display_meaning:
+                st.write(f"意味: {st.session_state.selected_word['意味']}")
+
+            
+
+        
+'''    if st.button('文学・哲学的なテーマ性') and st.session_state.selected_word['分類']=='文学・哲学的なテーマ性'！
         st.write('正解です。おめでとうございます！正確な意味も確認しましょう。')
         if st.button('意味を確認する'):
             st.session_state.display_meaning = True
