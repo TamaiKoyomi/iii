0041962c8300c8a73c473b6bad738a5d71e29546
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-07-17 07:00:38 +0000

    dgfh

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# Load the data
+@st.cache
+def load_data():
+    return pd.read_excel("四字熟語ガチャ.xlsx")
+
+words_df = load_data()
 
 def show_game():
     st.title('四字熟語カテゴリークイズ')
     st.write('aとbに分かれてクイズの正答率を競争します。最も正しいと思うものを選んでください。しかし、ChatGPTが分類したものなので違うと思っても怒らないでください。')
-
-    # Load the data
-    @st.cache
-    def load_data():
-        return pd.read_excel("四字熟語ガチャ.xlsx")
-
-    words_df = load_data()
 
     def judge(kategori):
         if st.session_state.selected_word['分類']==kategori:
@@ -73,4 +72,38 @@ def show_game():
                 st.write('正しい答えを確認し、この熟語をマスターしましょう！')
                 st.write(f"この熟語の意味: {st.session_state.selected_word['意味']}")
 
-show_game()
+def show_pro():
+    col1,col2,col3=st.colmuns(3)
+    with col1:
+        if st.button('文学・哲学的なテーマ性'):
+            for a in words_df:
+                if st.session_state.selected_word['分類'] == '文学・哲学的なテーマ性':
+                    st.header(f"単語名: {st.session_state.selected_word['単語']}")
+                    st.subheader(f"読み方：{st.session_state.selected_word['読み方']}")
+                    st.write(f"この熟語の意味: {st.session_state.selected_word['意味']}")
+                else:
+                    pass
+    with col2:
+        if st.button('行動・精神的な特性'):
+            for a in words_df:
+                if st.session_state.selected_word['分類'] == '行動・精神的な特性':
+                    st.header(f"単語名: {st.session_state.selected_word['単語']}")
+                    st.subheader(f"読み方：{st.session_state.selected_word['読み方']}")
+                    st.write(f"この熟語の意味: {st.session_state.selected_word['意味']}")
+                else:
+                    pass
+    with col3:
+        if st.button('自然・現象に関連するもの'):
+            for a in words_df:
+                if st.session_state.selected_word['分類'] == '自然・現象に関連するもの':
+                    st.header(f"単語名: {st.session_state.selected_word['単語']}")
+                    st.subheader(f"読み方：{st.session_state.selected_word['読み方']}")
+                    st.write(f"この熟語の意味: {st.session_state.selected_word['意味']}")
+                else:
+                    pass
+
+
+if st.sidebar.button('四字熟語クイズを解く'):
+    show_game()
+elif st.sidebar.button('四字熟語一覧を見る'):
+    show_pro()
