94b345e1f369547a7205bdc96f807699eb3df3e2
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-07-30 01:50:45 +0000

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -175,18 +175,28 @@ def ang_pro():
         st.subheader(f"四字熟語の意味:{st.session_state.selected_word['意味']}")
     
     yoji_list = list(st.session_state.selected_word['単語'])
+    ran_list =  yoji_list.copy
     ran_list = random.shuffle(yoji_list)
     
     col1,col2,col3,col4 = st.columns(4)
 
+    ans_list = []
+
     with col1:
-        st.button(yoji_list[0])
+        if st.button(yoji_list[0]):
+            ans_list.append(yoji_list[0])
     with col2:
-        st.button(yoji_list[1])
+        if st.button(yoji_list[1]):
+            ans_list.append(yoji_list[1])
     with col3:
-        st.button(yoji_list[2])
+        if st.button(yoji_list[2]):
+            ans_list.append(yoji_list[2])
     with col4:
-        st.button(yoji_list[3])
+        if st.button(yoji_list[3]):
+            ans_list.append(yoji_list[3])
+    st.subheader(ans_list)
+    
+    
 
 
 
