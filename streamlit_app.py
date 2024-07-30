e048d0666445e3dc486662fda28bfbc87273265b
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-07-30 02:51:41 +0000

    asfd

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,10 +171,10 @@ def ang_pro():
         st.session_state.selected_word = selected_word
         st.session_state.display_meaning = False
 
-        ans = ''
-
     if 'selected_word' in st.session_state:
         st.subheader(f"四字熟語の意味:{st.session_state.selected_word['意味']}")
+
+        ans = []
 
     def quiz():
         yoji_list = list(st.session_state.selected_word['単語'])
@@ -184,17 +184,17 @@ def ang_pro():
 
         with col1:
             if st.button(ran_list[0]):
-                return ans += ran_list[0]
+                return ans.append(ran_list[0])
         with col2:
             if st.button(ran_list[1]):
-                return ans += ran_list[1]
+                return ans.append(ran_list[1])
         with col3:
             if st.button(ran_list[2]):
-                return ans += ran_list[2]
+                return ans.append(ran_list[2])
         with col4:
             if st.button(ran_list[3]):
-                return ans += ran_list[3]
-            
+                return ans.append(ran_list[3])
+    
     st.write(quiz())
 
 
