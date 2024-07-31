7395f974de76b4965f2f351f681f7b1601562153
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-07-31 02:46:09 +0000

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,6 @@ def decide():
         st.session_state.selected_word = selected_word
         st.session_state.display_meaning = False
 
-        if 'ans' not in st.session_state:
-            st.session_state.ans = list()
-
 def yojiyoji():
     if 'ans' not in st.session_state:
         yoji_list = list(st.session_state.selected_word['単語'])
@@ -150,6 +147,10 @@ def ang_pro():
     ran_list = ranran()
 
     if 'selected_word' in st.session_state:
+
+        if 'ans' not in st.session_state:
+            st.session_state.ans = []
+
         st.subheader(f"四字熟語の意味:{st.session_state.selected_word['意味']}")
     
     col1,col2,col3,col4,col5 = st.columns(5)
