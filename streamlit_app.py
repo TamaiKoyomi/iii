935b1979c45ff89cd32a2e9b2578f070bd2c7521
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-08-04 07:22:38 +0000

    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,6 @@ def ranran():
     if 'ran_list' not in st.session_state:
         ran_list = random.sample(st.session_state.yoji_list,len(st.session_state.yoji_list))
         st.session_state.ran_list = ran_list
-        return st.session_state.ran_list
     else:
         pass
 
